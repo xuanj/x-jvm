27a47732ebedeaf2b6d7ffbce303e55ece8bb6e3
xuanj <xuanjian@live.com>
2016-05-19 00:29:41 +0800

    4vim

--- jvm-test/src/T2HeapCOOM.java
+++ jvm-test/src/T2HeapCOOM.java
@@ -1,17 +1,15 @@
-package t2;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class HeapCOOM {
+public class T2HeapCOOM {
 	
 	static class OOMObject{
 		
 	}
 
 	public static void main(String[] args) {
-		List<OOMObject> list = new ArrayList<OOMObject>();
-		while (true) {
+		List<OOMObject> list = new ArrayList<>();
+		while(true){
 			list.add(new OOMObject());
 		}
 
