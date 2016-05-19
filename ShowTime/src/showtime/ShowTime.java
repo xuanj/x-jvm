8b91b36dc2ac54593f2e425bc90bd9015e530dd2
xuanj <xuanjian@live.com>
2016-05-19 22:49:35 +0800

    Eclipse ShowTime 启动时间插件

---
+++ ShowTime/src/showtime/ShowTime.java
@@ -0,0 +1,27 @@
+package showtime;
+
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IStartup;
+
+public class ShowTime implements IStartup {
+
+	@Override
+	public void earlyStartup() {
+		Display.getDefault().syncExec(new Runnable() {
+			
+			@Override
+			public void run() {
+				long st = Long.parseLong(System.getProperty("eclipse.startTime"));
+				long ct = System.currentTimeMillis() - st;
+				Shell shell = Display.getDefault().getActiveShell();
+				String message = "Eclipsey启动耗时：" + ct + "ms";
+				MessageDialog.openInformation(shell, "Information", message );
+				
+			}
+		});
+
+	}
+
+}
